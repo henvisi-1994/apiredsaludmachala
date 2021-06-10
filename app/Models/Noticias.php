23fb9da9dc26c7f8e8389de91829bda4b44f3bb3
henvisi-1994 <henvisi_1994@hotmail.com>
2021-06-09 21:21:37 -0500

    Arreglo de Conflictos2

--- app/Models/Noticias.php
+++ app/Models/Noticias.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Noticias extends Model
 {
     use HasFactory;
+    protected $primaryKey = 'id_noticia';
     protected $fillable = [
         'titulo_noticia',
         'imagen_noticia',
